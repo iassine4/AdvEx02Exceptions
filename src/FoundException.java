ae1e3b19acd2b34d18778459df8d517a61e9a606
Yassine <yassine.zriba@fms-ea.com>
2025-12-19 12:00:01 +0100

    mettre en place une exception pour passer a l'instruction suivante

--- src/FoundException.java
+++ src/FoundException.java
@@ -1,17 +1,31 @@
+import java.util.Date;
+//import java.sql.Date;
 
 public class FoundException {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Instruction 1");
+		Date date = null;
+        Date today = new Date();
+        
+        try {
+        	System.out.println(date.getClass().getName());
+        }catch(Exception e) {
+        	System.out.println("Erreure la date est null, et je passe à l'instruction suivante");
+        }
+        
+        System.out.println(today.getClass().getName());
+        
+        
+        //Tester null
+   /*     if (date != null) {
+            System.out.println(date.getClass().getName());
+        } else {
+            System.out.println("date est null");
+        }
 
-		try {
-		    int result = 10 / 0;
-		} catch (Exception e) {
-			System.out.println(e.getClass());
-		}
-
-		System.out.println("Instruction 2");
+        System.out.println(today.getClass().getName());
+    */
 	}
 
 }
